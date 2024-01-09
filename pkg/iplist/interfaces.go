5d1dea76ab581ec154b0b72100970566f431fb6b
Yuzhou Liu <yuzhou.liu@mongodb.com>
2024-01-09 15:18:05 -0500

    remove interface

--- pkg/iplist/interfaces.go
+++
@@ -1,8 +0,0 @@
-package iplist
-
-// TODO delete this. This is just a reminder of the signature
-
-// IPGetter defines an interface for ip address providers to get source CIDRs
-type IPGetter interface {
-	GetIPs() ([]string, error)
-}
