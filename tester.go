704a70cb7b6bad40595138a32cc485199865215a
Yuzhou Liu <yuzhou.liu@mongodb.com>
2024-01-11 10:47:35 -0500

    with file lister

--- tester.go
+++ tester.go
@@ -2,33 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/kanopy-platform/argoslower/pkg/iplister"
-	"github.com/kanopy-platform/argoslower/pkg/iplister/decoder/github"
-	"github.com/kanopy-platform/argoslower/pkg/iplister/decoder/officeips"
-	"github.com/kanopy-platform/argoslower/pkg/iplister/reader/http"
+	filedeocder "github.com/kanopy-platform/argoslower/pkg/iplister/decoder/file"
+	"github.com/kanopy-platform/argoslower/pkg/iplister/reader/file"
 )
 
 func main() {
-	githubReader := http.New("https://api.github.com/meta")
-	githubDecoder := github.New()
+	// githubIpLister := iplister.New(
+	// 	http.New("https://api.github.com/meta"),
+	// 	github.New(),
+	// )
+	// ips, err := githubIpLister.GetIPs()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Github IPs: %v\n", ips)
 
-	githubIpLister := iplister.New(githubReader, githubDecoder)
-	ips, err := githubIpLister.GetIPs()
+	// officeipLister := iplister.New(
+	// 	http.New(os.Getenv("url"), http.WithBasicAuth(os.Getenv("user"), os.Getenv("pass"))),
+	// 	officeips.New(),
+	// )
+	// ips, err = officeipLister.GetIPs()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Office IPs: %v\n", ips)
+
+	file := iplister.New(
+		file.New("iplists.yaml"),
+		filedeocder.New("jira"),
+	)
+	ips, err := file.GetIPs()
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("Github IPs: %v\n", ips)
-
-	officeipReader := http.New(os.Getenv("url"), http.WithBasicAuth(os.Getenv("user"), os.Getenv("pass")))
-	officeipDecoder := officeips.New()
-	officeipLister := iplister.New(officeipReader, officeipDecoder)
-	ips, err = officeipLister.GetIPs()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Office IPs: %v\n", ips)
+	fmt.Printf("Jira IPs: %v\n", ips)
 }
