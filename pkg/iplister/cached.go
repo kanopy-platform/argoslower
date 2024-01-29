920707e0b4da489864b52649ee41670cb6a0c098
wwalter <53450727+whwalter@users.noreply.github.com>
2024-01-29 12:27:07 -0500

    cached ip lister and unit tests

---
+++ pkg/iplister/cached.go
@@ -0,0 +1,103 @@
+package iplister
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type CachedIPLister struct {
+	reader       Reader
+	decoder      Decoder
+	timeout      time.Duration
+	lastSync     time.Time
+	syncInterval time.Duration
+	lock         *sync.RWMutex
+	ips          []string
+}
+
+func NewCachedIPLister(reader Reader, decoder Decoder) *CachedIPLister {
+	out := &CachedIPLister{
+		reader:       reader,
+		decoder:      decoder,
+		timeout:      defaultTimeout,
+		syncInterval: 5 * time.Minute,
+		lock:         &sync.RWMutex{},
+	}
+	out.setIPs()
+	return out
+}
+
+func (i *CachedIPLister) SetTimeout(t time.Duration) {
+	if i == nil {
+		return
+	}
+	i.lock.Lock()
+	i.timeout = t
+	i.lock.Unlock()
+}
+
+func (i *CachedIPLister) GetIPs() ([]string, error) {
+	var out []string
+	var err error
+	syncNow, bgSync := i.needSync()
+
+	if syncNow {
+		err = i.setIPs()
+	}
+
+	ipl := i.getIPs()
+	out = make([]string, len(ipl))
+	copy(out, ipl)
+
+	if bgSync {
+		go i.setIPs()
+	}
+	return out, err
+}
+
+func (i *CachedIPLister) needSync() (bool, bool) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	if i.lastSync.IsZero() {
+		return true, false
+	}
+
+	if time.Now().Sub(i.lastSync) >= i.syncInterval {
+		return false, true
+	}
+
+	return false, false
+}
+func (i *CachedIPLister) getIPs() []string {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
+	return i.ips
+}
+
+func (i *CachedIPLister) setIPs() error {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	// always update the sync time to keep from overwhelming the reader target
+	i.lastSync = time.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), i.timeout)
+	defer cancel()
+
+	reader, err := i.reader.Data(ctx)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	ipList, err := i.decoder.Decode(reader)
+	if err != nil {
+		return err
+	}
+
+	if err := ValidateCIDRs(ipList); err != nil {
+		return err
+	}
+
+	i.ips = ipList
+	return nil
+}
--- pkg/iplister/iplister_test.go
+++ pkg/iplister/iplister_test.go
@@ -2,6 +2,7 @@ package iplister
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"strings"
 	"testing"
@@ -104,4 +105,134 @@ func TestValidateCIDRs(t *testing.T) {
 		err := ValidateCIDRs(test.input)
 		assert.Equal(t, test.wantErr, err != nil, test.name)
 	}
+}
+
+func TestNewCachedIPLister(t *testing.T) {
+	t.Parallel()
+
+	ipl := NewCachedIPLister(&mockReader{}, &mockDecoder{})
+	assert.Equal(t, defaultTimeout, ipl.timeout)
+
+	ipl.SetTimeout(500 * time.Minute)
+	assert.Equal(t, (500 * time.Minute), ipl.timeout)
+}
+
+func TestCachedIPListerGetIPs(t *testing.T) {
+	t.Parallel()
+
+	fakeData := "1.2.3.4/32"
+	decoderData := []string{fakeData}
+
+	mr := &mockReader{
+		ret: fakeData,
+		err: nil,
+	}
+	md := &mockDecoder{
+		ret: decoderData,
+		err: nil,
+	}
+	ipl := NewCachedIPLister(mr, md)
+
+	// Test ip list population at creation time
+	ipl.lock.Lock()
+	assert.Equal(t, 1, md.count)
+	ipl.lock.Unlock()
+
+	// Test ip list is returned from cache
+	ips, err := ipl.GetIPs()
+	assert.NoError(t, err)
+	assert.Contains(t, ips, "1.2.3.4/32")
+
+	ipl.lock.Lock()
+	assert.Equal(t, 1, md.count)
+	ipl.lock.Unlock()
+
+	// Test ip list is returned from cache
+	ipl.lock.Lock()
+	md.ret = []string{"2.3.4.5/32"}
+	ipl.lock.Unlock()
+	ips, err = ipl.GetIPs()
+	assert.NoError(t, err)
+	assert.NotContains(t, ips, "2.3.4.5/32")
+
+	ipl.lock.Lock()
+	assert.Equal(t, 1, md.count)
+	ipl.lock.Unlock()
+
+	// Test 5min background sync for stale ip lists
+	ipl.lock.Lock()
+	ipl.lastSync = ipl.lastSync.Add(-5 * time.Minute)
+	ipl.lock.Unlock()
+
+	ips, err = ipl.GetIPs()
+	assert.NoError(t, err)
+
+	ipl.lock.Lock()
+	index := md.count
+	ipl.lock.Unlock()
+
+	for !contains(ips, "2.3.4.5/32") {
+		ips, err = ipl.GetIPs()
+		assert.NoError(t, err)
+		index += 1
+
+		if index >= 1000 {
+			break
+		}
+	}
+
+	assert.True(t, contains(ips, "2.3.4.5/32"))
+	ipl.lock.RLock()
+	assert.True(t, mr.count <= index)
+	assert.True(t, md.count <= index)
+	ipl.lock.RUnlock()
+
+	// Test failed bg sync retains stale ip list
+	ipl.lock.Lock()
+	md.ret = []string{"3.4.5.6/32"}
+	mr.err = fmt.Errorf("slow down")
+	ipl.lastSync = ipl.lastSync.Add(-5 * time.Minute)
+	ipl.lock.Unlock()
+
+	index = 0
+	for ; index <= 100; index++ {
+		ips, err = ipl.GetIPs()
+		assert.NoError(t, err)
+		assert.True(t, !contains(ips, "3.4.5.6/32"))
+	}
+
+	assert.True(t, !contains(ips, "3.4.5.6/32"))
+	ipl.lock.RLock()
+	assert.True(t, mr.count <= index)
+	assert.True(t, md.count <= index)
+	ipl.lock.RUnlock()
+
+	// Test bg sync recovers from intermitant error
+	ipl.lock.Lock()
+	mr.err = nil
+	ipl.lastSync = ipl.lastSync.Add(-5 * time.Minute)
+	ipl.lock.Unlock()
+
+	index = 0
+
+	for !contains(ips, "3.4.5.6/32") {
+		ips, err = ipl.GetIPs()
+		assert.NoError(t, err)
+		index += 1
+
+		if index >= 1000 {
+			break
+		}
+	}
+
+	assert.True(t, contains(ips, "3.4.5.6/32"))
+}
+
+func contains(in []string, val string) bool {
+	for _, v := range in {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
