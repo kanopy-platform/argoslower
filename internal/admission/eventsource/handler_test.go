32f2ab105756a5f6a3b5b9a1b0b139cac4b13134
wwalter <53450727+whwalter@users.noreply.github.com>
2024-02-02 16:53:18 -0500

    remove cruft

--- internal/admission/eventsource/handler_test.go
+++ internal/admission/eventsource/handler_test.go
@@ -169,38 +169,6 @@ func TestEventSourceHandler(t *testing.T) {
 	}
 }
 
-/*
-func TestValidateWebhookEventSource(t *testing.T) {
-	tests := map[string]struct {
-		spec *esv1alpha1.WebhookEventSource
-		err  bool
-	}{
-		"noauth": {
-			spec: &esv1alpha1.WebhookEventSource{},
-			err:  true,
-		},
-		"valid": {
-			spec: &esv1alpha1.WebhookEventSource{
-				WebhookContext: esv1alpha1.WebhookContext{
-					AuthSecret: &corev1.SecretKeySelector{},
-				},
-			},
-		},
-	}
-
-	for name, test := range tests {
-		e := validateWebhookEventSource(test.spec)
-
-		if test.err {
-			assert.Error(t, e, name)
-		} else {
-			assert.NoError(t, e, name)
-		}
-	}
-
-}
-*/
-
 func TestValidateGithubEventSource(t *testing.T) {
 
 	tests := map[string]struct {
@@ -328,34 +296,3 @@ func TestValidateGithubEventSource(t *testing.T) {
 	}
 
 }
-
-/*
-func TestValidateGithubEventSource(t *testing.T) {
-
-	tests := map[string]struct {
-		spec *esv1alpha1.GithubEventSource
-		err  bool
-	}{
-		"noauth": {
-			spec: &esv1alpha1.GithubEventSource{},
-			err:  true,
-		},
-		"valid": {
-			spec: &esv1alpha1.GithubEventSource{
-				WebhookSecret: &corev1.SecretKeySelector{},
-			},
-		},
-	}
-
-	for name, test := range tests {
-		e := validateGithubEventSource(test.spec)
-
-		if test.err {
-			assert.Error(t, e, name)
-		} else {
-			assert.NoError(t, e, name)
-		}
-	}
-
-}
-*/
